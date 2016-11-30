a5f281c299ab5a403ff36d481a5b1ef720472798
LP6m <lp6m2014@gmail.com>
2016-12-01 02:52:02 +0900

    add search form and result view

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -2,22 +2,29 @@ package controllers;
 import mercariapi.*;
 
 import play.*;
+import play.data.*;
 import play.mvc.*;
+import static play.data.Form.*;
 import java.util.*;
 import views.html.*;
 
 public class Application extends Controller {
 	public static MercariAPI mercariapi;
     public static Result index() {
-		mercariapi = new MercariAPI();
-		System.out.println(mercariapi.access_token);
-		List<MercariItem> res = mercariapi.GetAllItemsWithSellers("220249289",new ArrayList<Integer>(Arrays.asList(1,2,3)));
-		for(MercariItem item : res){
-			System.out.println(item.name + " " + item.id);
-		}
-																  
-										  
         return ok(index.render("Your new application is ready."));
     }
+	public static class SearchForm{
+		public String sellerid;
+	}
+ 	public static Result searchresult(){
+		Form<SearchForm> searchform = new Form<SearchForm>(SearchForm.class).bindFromRequest();
+		String sellerid = searchform.get().sellerid; //"220249289"
+		System.out.println(sellerid);
+		mercariapi = new MercariAPI();
+		System.out.println(mercariapi.access_token);
+		List<MercariItem> res = mercariapi.GetAllItemsWithSellers(sellerid,new ArrayList<Integer>(Arrays.asList(2,3)));
+		return ok(searchresult.render(res));
+		
+	}
 
 }
