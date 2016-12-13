4f77f0a1b0479c95a257e027b427a2d0c8cc8473
LP6m <lp6m2014@gmail.com>
2016-12-14 02:48:41 +0900

    additemとedititemを作成（未完成）

--- app/controllers/Application.java
+++ app/controllers/Application.java
@@ -12,8 +12,11 @@ import actions.*;
 import models.*;
 public class Application extends Controller {
 	public static MercariSearcher mercariapi;
+
+	@With(BasicAuthAction.class)
     public static Result index() {
-	    return ok(index.render("Your new application is ready."));
+		String pop_message = session("message") == null ? "" : session("message");
+	    return ok(index.render(pop_message));
     }
 	public static class SearchForm{
 		public String sellerid;
@@ -29,6 +32,7 @@ public class Application extends Controller {
 		mercariapi.GetItemInfobyItemID("m170271875"); 
 		return ok(searchresult.render(res));		
 	}
+	
 	@With(BasicAuthAction.class)
 	public static Result createuser(){
 		/*adminでないとindexに送り返す*/
@@ -50,13 +54,11 @@ public class Application extends Controller {
 			user.save();
 			return redirect("/");
 		}
-		
 	}
 
 	@With(BasicAuthAction.class)
 	public static Result login(){/*ログインページを表示するだけ*/
-		Form<LoginForm> loginForm = new Form<LoginForm>(LoginForm.class);
-        return ok(login.render("login",loginForm));
+        return ok(login.render("login"));
 	}
 	@With(BasicAuthAction.class)
 	public static Result authenticate(){
@@ -71,18 +73,66 @@ public class Application extends Controller {
 				if(user.phpssid != null) session("phpssid",user.phpssid);
 				return redirect("/");
 			}
-			return badRequest(login.render("ERROR",new Form<LoginForm>(LoginForm.class)));
+			return badRequest(login.render("ERROR"));
 		}catch(Exception e){
-			return badRequest(login.render("ERROR",new Form<LoginForm>(LoginForm.class)));
+			return badRequest(login.render("ERROR"));
 		}
 	}
 
 	@With(BasicAuthAction.class)
 	public static Result additem(){
-		
+		/*ログイン必須*/
+		if(session("username")==null){
+			session("message","ログインしてください");
+			return redirect("/");
+		}
+		//POSTのみ
+		try{
+			Map<String,String[]> f = request().body().asFormUrlEncoded();
+			String itemid = f.get("itemid")[0];
+			Integer zaikonum = Integer.parseInt(f.get("zaikonum")[0]);
+			/*商品IDから商品の情報を検索*/
+			MercariSearcher s = new MercariSearcher();
+			MercariItem item = s.GetItemInfobyItemID(itemid);
+			if(item != null){
+				String warnstr = "";
+				/*まずその商品を即時削除する.*/
+				MercariExhibitter me = new MercariExhibitter(session("phpssid"));
+				Boolean cancel_rst = me.Cancel(itemid);
+				if(cancel_rst == false){ /*他人の商品の場合は削除に失敗する(はず)*/
+					warnstr = "警告: 商品の削除に失敗（他人の商品？）";
+				}
+				/*即時商品の出品*/
+				MercariExhibitItem new_item = new MercariExhibitItem(item);
+				me.Sell(new_item);
+				/*商品を管理データベースに追加する*/
+				ManageItem manageitem = new ManageItem();
+				manageitem.itemid = itemid;
+				manageitem.item = item;
+				manageitem.username = session("username");
+				manageitem.zaiko = zaikonum;
+				manageitem.ignoreflag = false;
+				manageitem.save();
+				session("message","商品を追加しました" + warnstr);
+				return redirect("/");
+			}else{
+				/*商品が見つからなかった*/
+				session("message","商品が見つからなかったため,商品を追加できませんでした");
+				return redirect("/");
+			}
+		}catch(Exception e){
+			session("message","商品の追加に失敗しました");
+			return redirect("/");
+		}
 	}
 	@With(BasicAuthAction.class)
 	public static Result deleteitem(){
-		
+		//POSTのみ
+		try{
+			return redirect("/");
+		}catch(Exception e){
+			return redirect("/");
+		}
 	}
 }
+	
