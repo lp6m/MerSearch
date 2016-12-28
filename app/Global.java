d006a2065fafd577b88a2fc8c51355289efd36cf
LP6m <lp6m2014@gmail.com>
2016-12-29 04:23:52 +0900

    エラーページを作成

--- app/Global.java
+++ app/Global.java
@@ -1,7 +1,20 @@
 import play.*;
 import play.data.*;
 import play.mvc.*;
+import play.mvc.Results.*;
+import play.mvc.Results.Status;
 import jobs.*;
+import play.libs.*;
+import views.*;
+import play.mvc.Http.RequestHeader;
+
+import play.*;
+import play.mvc.*;
+import play.mvc.Http.*;
+import play.libs.F.*;
+
+import static play.mvc.Results.*;
+
 public class Global extends GlobalSettings{
 	@Override
 	public void onStart(Application app){
@@ -11,4 +24,17 @@ public class Global extends GlobalSettings{
 	public void onStop(Application app){
 		MyTaskActorBase.getInstance().shutdown();
 	}
+	@Override
+	public Promise<SimpleResult> onError(RequestHeader request, Throwable t) {
+		return Promise.<SimpleResult> pure(internalServerError(
+            views.html.error.render()
+        ));
+	}
+
+	@Override
+	public Promise<SimpleResult> onHandlerNotFound(RequestHeader request) {
+        return Promise.<SimpleResult>pure(notFound(
+            views.html.notFoundPage.render()
+        ));
+    }
 }
